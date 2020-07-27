9ea2ef0b8cdfb091cc274142e468873f55427d1b
simran-s4460 <66850200+simran-s4460@users.noreply.github.com>
2020-07-27 00:12:15 -0700

    Update ConnectionProvider.java

--- src/project/ConnectionProvider.java
+++ src/project/ConnectionProvider.java
@@ -7,7 +7,7 @@ public class ConnectionProvider {
 
     public static Connection getCon() {
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?autoReconnect=true&useSSL=false", "root", "Smart@786");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library?autoReconnect=true&useSSL=false", "root", "root");
             return con;
         } catch (Exception e) {
             System.out.println(e);
